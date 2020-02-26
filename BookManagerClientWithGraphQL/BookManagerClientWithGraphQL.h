f5bb6f4db91c2d7bd5dbabe8bbd88a55d302caa1
ikawashima41 <i.kawashima41@gmail.com>
2020-02-26 10:29:33 +0900

    Initial Commit

---
+++ BookManagerClientWithGraphQL/BookManagerClientWithGraphQL.h
@@ -0,0 +1,19 @@
+//
+//  BookManagerClientWithGraphQL.h
+//  BookManagerClientWithGraphQL
+//
+//  Created by Iichiro Kawashima on 2020/02/26.
+//  Copyright © 2020 Iichiro Kawashima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BookManagerClientWithGraphQL.
+FOUNDATION_EXPORT double BookManagerClientWithGraphQLVersionNumber;
+
+//! Project version string for BookManagerClientWithGraphQL.
+FOUNDATION_EXPORT const unsigned char BookManagerClientWithGraphQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BookManagerClientWithGraphQL/PublicHeader.h>
+
+
